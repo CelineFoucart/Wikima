a160e3a6b1b23790981374b7f977218a701afbc1
Céline Foucart <celinefoucart@yahoo.fr>
2023-01-30 22:19:06 +0100

    fix: lettre accentuée dans tri

--- src/Service/AlphabeticalHelperService.php
+++ src/Service/AlphabeticalHelperService.php
@@ -9,14 +9,14 @@ class AlphabeticalHelperService
         $orderedItems = [];
         
         foreach ($items as $item) {
-            $title = strtolower($item->getTitle());
+            $title = mb_strtolower($item->getTitle());
             $title = preg_replace('#é|è|ë|ê#', 'e', $title);
             $title = preg_replace('#â|à|ä#', 'a', $title);
             $title = preg_replace('#ù|ü|û#', 'u', $title);
             $title = preg_replace('#ï|î#', 'i', $title);
             $title = preg_replace('#ô|ö#', 'o', $title);
 
-            $letter = strtoupper(substr(trim($item, ' '), 0, 1));
+            $letter = strtoupper(substr(trim($title, ' '), 0, 1));
 
             if (isset($orderedItems[$letter])) {
                 $orderedItems[$letter][] = $item;
