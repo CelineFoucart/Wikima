4eb7dbc238db13fa41388db3d29899cc9c720956
Céline Foucart <celinefoucart@yahoo.fr>
2023-07-01 15:41:18 +0200

    add: admin Overview

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,17 +2,47 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\About;
+use App\Form\AboutType;
+use App\Repository\AboutRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin', name: 'admin_app_dashboard')]
+    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_SUPER_ADMIN') or is_granted('ROLE_EDITOR')")]
     public function dashboardAction(): Response
     {
         return $this->render('admin/dashboard.html.twig', [
             
+        ]);
+    }
+
+    #[Route('/admin/about', name: 'admin_app_overview')]
+    #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_SUPER_ADMIN')")]
+    public function overviewAction(Request $request, AboutRepository $aboutRepository): Response
+    {
+        $overview = $aboutRepository->findAboutRow('overview');
+        if (null === $overview) {
+            $overview = (new About())->setType('overview');
+        }
+
+        $form = $this->createForm(AboutType::class, $overview);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $aboutRepository->add($overview);
+            $this->addFlash('success', 'Les modifications ont été enregistrées.');
+
+            return $this->redirectToRoute('admin_app_overview');
+        }
+
+        return $this->renderForm('Admin/overview.html.twig', [
+            'form' => $form,
         ]);
     }
 }
