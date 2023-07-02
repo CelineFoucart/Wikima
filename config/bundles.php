931a2424357e9843145f9316e749834301e52519
Céline Foucart <celinefoucart@yahoo.fr>
2023-07-02 18:43:19 +0200

    add: admin backup

--- config/bundles.php
+++ config/bundles.php
@@ -21,12 +21,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
-    Sonata\Form\Bridge\Symfony\SonataFormBundle::class => ['all' => true],
-    Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
-    Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
-    Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
-    Sonata\Twig\Bridge\Symfony\SonataTwigBundle::class => ['all' => true],
-    Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
---
+++ src/Controller/Admin/AdminBackupController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Backup;
+use App\Repository\BackupRepository;
+use App\Service\BackupService;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+#[Route('/admin/backup')]
+#[Security("is_granted('ROLE_SUPER_ADMIN')")]
+final class AdminBackupController extends AbstractAdminController
+{
+    public function __construct(
+        private BackupService $backupService,
+        private BackupRepository $backupRepository
+    ) {
+    }
+
+    #[Route('/', name: 'admin_app_backup_list', methods:['GET'])]
+    public function listAction(): Response
+    {
+        return $this->render('admin/backup.html.twig', [
+            'backups' => $this->backupRepository->findAll(),
+        ]);
+    }
+    
+    #[Route('/create', name: 'admin_app_backup_create', methods:['GET'])]
+    public function createAction(): Response
+    {
+        $filename = $this->backupService->save()->getFilename();
+
+        if (null === $filename) {
+            $this->addFlash('error', join('<br>', $this->backupService->getErrors()));
+        } else {
+            $backup = (new Backup())
+                ->setFilename($filename)
+                ->setCreatedAt($this->backupService->getDate())
+            ;
+            $this->backupRepository->add($backup);
+
+            $this->addFlash( 'success', 'La sauvegarde de la base de donnée a été réalisée avec succès');
+        }
+
+        return $this->redirectToRoute('admin_app_backup_list');
+    }
+
+    #[Route('/{id}/download', name: 'admin_app_backup_download', methods:['GET'])]
+    public function downloadAction(Backup $backup): Response
+    {
+        $folder = $this->backupService->getBackupFolder();
+        $file = $folder.DIRECTORY_SEPARATOR.$backup->getFilename();
+
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $backup->getFilename()
+        );
+
+        return $response;
+    }
+
+    #[Route('/{id}/delete', name: 'admin_app_backup_delete', methods:['POST'])]
+    public function deleteAction(Request $request, Backup $backup): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$backup->getId(), $request->request->get('_token'))) {
+            $path = $this->backupService->getBackupFolder() . DIRECTORY_SEPARATOR . $backup->getFilename();
+
+            if (file_exists($path)) {
+                unlink($path);
+            }
+
+            $this->backupRepository->remove($backup, true);
+            $this->addFlash('success', "La sauvegarde a été supprimée avec succès.");
+        }
+
+        return $this->redirectToRoute('admin_app_backup_list', [], Response::HTTP_SEE_OTHER);
+    }
+}
