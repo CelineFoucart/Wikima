e6560ef2a0c2c164ebf0d644c07932417e62ac81
Céline Foucart <celinefoucart@yahoo.fr>
2023-07-22 18:53:30 +0200

    add: gestion galerie article langue

--- src/Controller/Admin/Idiom/AdminIdiomArticleController.php
+++ src/Controller/Admin/Idiom/AdminIdiomArticleController.php
@@ -3,16 +3,21 @@
 namespace App\Controller\Admin\Idiom;
 
 use App\Entity\Idiom;
-use App\Entity\IdiomArticle;
-use App\Form\IdiomArticleType;
-use App\Repository\IdiomArticleRepository;
+use App\Entity\Image;
 use DateTimeImmutable;
+use App\Entity\IdiomArticle;
+use App\Form\Admin\ImageType;
+use App\Form\IdiomArticleType;
+use App\Entity\Data\SearchData;
+use App\Form\AdvancedSearchType;
+use App\Repository\IdiomArticleRepository;
+use App\Repository\ImageRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/idiom')]
 #[Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_EDITOR')")]
@@ -68,6 +73,56 @@ class AdminIdiomArticleController extends AbstractController
         ]);
     }
 
+    #[Route('/articles/{id}/gallery', name: 'admin_app_idiom_article_gallery', methods: ['GET', 'POST'])]
+    public function galleryAction(IdiomArticle $article, Request $request, ImageRepository $imageRepository): Response
+    {
+        $page = $request->query->getInt('page', 1);
+        $image = (new Image())->setPortals($article->getIdiom()->getPortals());
+        $formImage = $this->createForm(ImageType::class, $image);
+        $formImage->handleRequest($request);
+        
+        if ($formImage->isSubmitted()) { 
+            if ($formImage->isValid()) {
+                $imageRepository->add($image, true);
+                $article->addImage($image);
+                $imageRepository->add($image, true);
+                $this->idiomArticleRepository->add($article, true);
+                $this->addFlash('success', "L'image a bien été ajoutée.");
+                return $this->redirectToRoute('admin_app_idiom_article_gallery',  ['id' => $article->getId()]);
+
+            } else {
+                $this->addFlash('error',  "La soumission a échoué, car le formulaire n'est pas valide.");
+            }
+        } elseif ('POST' === $request->getMethod()) {
+            $this->handleGallery($request, $article, $imageRepository);
+
+            return $this->redirectToRoute('admin_app_idiom_article_gallery',  ['id' => $article->getId()]);
+        }
+
+        $excludes = array_map(function (Image $item) {
+            return $item->getId();
+        }, $article->getImages()->toArray());
+
+        $searchData = (new SearchData())->setPage($page);
+        $form = $this->createForm(AdvancedSearchType::class, $searchData, ['allow_extra_fields' => true]);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $images = $imageRepository->search($searchData, $excludes, 15);
+        } else {
+            $images = $imageRepository->findPaginated($page, $excludes, 15);
+        }
+        
+        return $this->render('Admin/idiom_article/gallery.html.twig', [
+            'idiom' => $article->getIdiom(),
+            'article' => $article,
+            'images' => $images,
+            'section_active' => true,
+            'formImage' => $formImage->createView(),
+            'form' => $form->createView(),
+        ]);
+    }
+
     #[Route('/{id}/delete', name: 'admin_app_idiom_article_delete', methods: ['POST'])]
     public function deleteAction(Request $request, IdiomArticle $idiomArticle): Response
     {
@@ -92,6 +147,30 @@ class AdminIdiomArticleController extends AbstractController
 
         if (null !== $request->get('btn_save_and_edit')) {
             return $this->redirectToRoute('admin_app_idiom_article_edit', ['id' => $idiomArticle->getId()]);
+        }
+    }
+
+    private function handleGallery(Request $request, IdiomArticle $article, ImageRepository $imageRepository): void
+    {
+        $imageId = $request->request->getInt('imageId');
+        $image = $imageRepository->find($imageId);
+
+        if (null === $image) {
+            $this->addFlash('error', "L'image que vous avez choisi n'existe pas.");
+        }
+
+        $delete = $request->request->get('delete');
+
+        if ($this->isCsrfTokenValid('image'.$image->getId(), $request->request->get('_token'))) {
+            if (null === $delete) {
+                $article->addImage($image);
+                $this->idiomArticleRepository->add($article);
+                $this->addFlash('success', "L'image a bien été ajoutée à l'article.");
+            } else {
+                $article->removeImage($image);
+                $this->idiomArticleRepository->add($article);
+                $this->addFlash('success', "L'image a bien été enlevée de l'article.");
+            }
         }
     }
 }
