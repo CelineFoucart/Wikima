d89af2a565628e901732b7c9e6867ebacb25be21
Céline Foucart <celinefoucart@yahoo.fr>
2023-12-28 22:34:02 +0100

    add: ajout d'une image à la carte

--- src/Controller/Admin/Place/AdminMapController.php
+++ src/Controller/Admin/Place/AdminMapController.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Controller\Admin\Place;
 
 use App\Entity\Map;
+use App\Entity\Data\SearchData;
+use App\Form\Admin\MapFormType;
 use App\Repository\MapRepository;
+use App\Form\Search\AdvancedSearchType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\Admin\AbstractAdminController;
-use App\Form\Admin\MapFormType;
+use App\Repository\ImageRepository;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -56,8 +59,51 @@ class AdminMapController extends AbstractAdminController
     #[Route('/{id}/show', name: 'admin_app_map_show', methods:['GET'])]
     public function showAction(Map $map): Response
     {
-        return $this->render('Admin/map/show.html.twig', [
+        return $this->render('Admin/map/show_general.html.twig', [
             'map' => $map,
+            'general_active' => true,
+        ]);
+    }
+
+    #[Route('/{id}/image', name: 'admin_app_map_image', methods:['GET', 'POST'])]
+    public function imageAction(Request $request, Map $map, ImageRepository $imageRepository): Response
+    {
+        $page = $request->query->getInt('page', 1);
+        $excludes = [];
+        if ($map->getImage()) {
+            $excludes[] = $map->getImage()->getId();
+        }
+
+        if ($request->isMethod('POST')) {
+            $imageId = $request->request->getInt('imageId');
+            $image = $imageRepository->find($imageId);
+
+            if (null === $image) {
+                $this->addFlash('error', "L'image que vous avez choisi n'existe pas.");
+            } else {
+                $map->setImage($image);
+                $this->mapRepository->save($map, true);
+                $this->addFlash('success', "L'image a bien été ajoutée.");
+            }
+
+            return $this->redirectToRoute('admin_app_map_show', ['id' => $map->getId()]);
+        }
+
+        $searchData = (new SearchData())->setPage($page);
+        $searchForm = $this->createForm(AdvancedSearchType::class, $searchData, ['allow_extra_fields' => true]);
+        $searchForm->handleRequest($request);
+
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $images = $imageRepository->search($searchData, $excludes, 15);
+        } else {
+            $images = $imageRepository->findPaginated($page, $excludes, 15);
+        }
+
+        return $this->render('Admin/map/show_image.html.twig', [
+            'map' => $map,
+            'images' => $images,
+            'searchForm' => $searchForm->createView(),
+            'image_active' => true,
         ]);
     }
 
